f05890d749ce3bd98f2d064e34352de9ae731ae9
Masayuki Komatsu <komatsu-m@gnavi.co.jp>
2018-03-01 16:06:52 +0900

    add tree search

---
+++ tree-search.py
@@ -0,0 +1,93 @@
+data = [
+    {
+        'id': 1,
+        'children': [
+            {
+                'id': 8,
+                'children': []
+            }
+        ]
+    },
+    {
+        'id': 2,
+        'children': [
+            {
+                'id': 3,
+                'children': [
+                    {
+                        'id': 4,
+                        'children': []
+                    },
+                    {
+                        'id': 7,
+                        'children': []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        'id': 5,
+        'children': [
+            {
+                'id': 6,
+                'children': []
+            }
+        ]
+    }
+]
+
+#breadth first search
+stack = data
+safe = 0
+visited = []
+while stack:
+    print(",".join(str(_['id']) for _ in stack))
+    current = stack[0]
+    visited.append(current)
+    stack.pop(0)
+    if len(current['children']) > 0:
+        stack.extend(current['children'])
+
+print (",".join([str(v['id']) for v in visited]))
+
+#depth first search
+stack = data
+safe = 0
+visited = []
+while stack:
+    print(",".join(str(_['id']) for _ in stack))
+    current = stack[0]
+    visited.append(current)
+    stack.pop(0)
+    if len(current['children']) > 0:
+        for a in current['children']:
+            stack.insert(0, a)
+
+
+    if safe > 50:
+        break
+    safe = safe + 1
+
+print (",".join([str(v['id']) for v in visited]))
+
+# depath first search
+def dfs(list):
+    for _ in list:
+        print (_)
+        if len(_['children']) > 0:
+            dfs(_['children'])
+dfs (data)
+
+
+# breadth first search
+def bfs (list):
+    stack = []
+    for _ in list:
+        print(str(_['id']) + '->')
+        if len(_['children']) > 0:
+            stack.extend(_['children'])
+
+    if len(stack) > 0:
+        bfs(stack)
+bfs(data)
